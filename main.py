0e2b8dafb2e290a6e49bf40da4ff6073a2da73f5
naasamajh1 <158548756+naasamajh1@users.noreply.github.com>
2025-04-12 10:41:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
     socketio.run(
         app,
-        host='127.0.0.1',
+        host='0.0.0.0',
         port=port,
         debug=app.config['DEBUG'],
         use_reloader=app.config['DEBUG']
